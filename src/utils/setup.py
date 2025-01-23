0fe43a0c26b7b7d1afa4eb9fba0682211d874769
piotrek-piotrek-dev <piotrek.piotrowski.dev@gmail.com>
2025-01-23 10:47:04 +0100

    added setup script

---
+++ src/utils/setup.py
@@ -0,0 +1,47 @@
+"""
+this script is for setting up your environment to implement the DeepSort algorithm for person detection using open Vino
+credits and sources from: https://docs.openvino.ai/2024/notebooks/person-tracking-with-output.html#select-inference-device
+"""
+
+import subprocess
+import sys
+
+PACKAGES_TO_INSTALL = [
+    "openvino>=2024.0.0",
+    "opencv-python",
+    "requests",
+    "scipy",
+    "tqdm",
+    "matplotlib>=3.4",
+]
+
+def is_package_installed(packageName: str) -> bool:
+    if packageName in sys.modules:
+        print(f"package {packageName} already installed\n")
+        return True
+    else:
+        print(f"package {packageName} not installed, attempting to install\n")
+        return False
+
+def install_dependencies() -> bool:
+    for package in PACKAGES_TO_INSTALL:
+        if not is_package_installed(package):
+            if not install_package(package): return False
+    return True
+
+def install_package(packageName: str) -> bool:
+    try:
+        print(f"Installing {packageName}...\n")
+        ret = subprocess.check_call([sys.executable, "-m", "pip", "install", packageName])
+        if ret == 0:
+            print(f"package {packageName} is installed\n")
+            return True
+    except subprocess.CalledProcessError as e:
+        print(f"something went wrong with installing {packageName}\n"
+              f"check output: {e.output}")
+        return False
+
+
+if __name__ == '__main__':
+    if not install_dependencies():
+        sys.exit(1)

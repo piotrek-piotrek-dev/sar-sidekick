0df31124f708d0d3a5d410d17ba509e8ae5f55a4
piotrek-piotrek-dev <piotrek.piotrowski.dev@gmail.com>
2025-01-31 01:16:28 +0100

    converting time measurements to nano seconds

--- src/helpers/TimeMeasurements.py
+++ src/helpers/TimeMeasurements.py
@@ -1,4 +1,5 @@
 """
+all return values are in nano seconds
 a decorator - measure_exec_time to add to any method, example:
 
 @measure_exec_time
@@ -25,27 +26,27 @@ print(timer.exec_time)
 """
 
 from functools import wraps
-from time import perf_counter
+from time import perf_counter_ns
 from typing import Any
 
 
 def measure_exec_time(func):
     @wraps(func)
     def wrap(*args, **kwargs) -> (Any, float):
-        time_start = perf_counter()
+        time_start = perf_counter_ns()
         result = func(*args, **kwargs)
         # print('func:%r args:[%r, %r] took: %2.4f sec' % \
         #       (func.__name__, args, kwargs, time_end - time_start))
-        return result, perf_counter() - time_start
+        return result, perf_counter_ns() - time_start
     return wrap
 
 
 class MeasureExecTime:
     def __enter__(self):
-        self.start = perf_counter()
+        self.start = perf_counter_ns()
         return self
 
     def __exit__(self, type, value, traceback):
-        self.exec_time = perf_counter() - self.start
+        self.exec_time = perf_counter_ns() - self.start
         # self.readout = f'Time: {self.time:.3f} seconds'
         # print(self.readout)
